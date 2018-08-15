b10a54eaee7139eb68f6f94ad30ecd98305094d5
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-08-15 17:32:34 -0500

    heroku deployment checklist

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
+  res.send({ hi: `req:${res}` })
 });
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
