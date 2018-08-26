3f1e8a68cf4a16736f322f5af31b99a1c2d54003
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-08-25 19:23:24 -0500

    OAuth Callbacks

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ app.get(
   '/auth/google', 
   passport.authenticate('google', {
   scope: ['profile', 'email'],
-}))
+}));
+
+app.get(
+  '/auth/google/callback', 
+  passport.authenticate('google')
+);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
