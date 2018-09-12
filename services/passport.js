3fdadf6aa448ed7f2905d59e2f6f5a9b5cd860fa
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-12 16:47:24 -0500

    async await

--- services/passport.js
+++ services/passport.js
@@ -13,20 +13,19 @@ passport.deserializeUser((id, done) => {
     .then(user => done(null, user))
 })
 
-passport.use(new GoogleStrategy({
+passport.use(
+  new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true,
-}, (accessToken, refreshToken, profile, done ) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        done(null, existingUser);
-      }else {
-        new User({googleId: profile.id})
-          .save()
-          .then(user => done(null, user));
-      }
-    })
+},
+async (accessToken, refreshToken, profile, done ) => {
+  const existingUser=  await User.findOne({ googleId: profile.id });
+  
+    if (existingUser) {
+      return done(null, existingUser);
+    }
+    const user = await new User({googleId: profile.id}).save()
+    done(null, user);
 }));
