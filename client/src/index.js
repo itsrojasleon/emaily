b614aeaa9cf7278add3ea5b4e019a592d82499ab
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-13 14:46:52 -0500

    Finishing reducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
+import reducers from './reducers';
+
 import App from './components/App';
 
 const store = createStore(
-  () => [],
+  reducers,
   applyMiddleware()
 );
 
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,1 +1,6 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+
+export default combineReducers({
+  auth: authReducer,
+});
