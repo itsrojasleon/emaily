2bae6f00297e076287316e74b30f08c3733a9503
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-14 17:31:42 -0500

    AuthReducer return values

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
-  
-  dispatch({ type: FETCH_USER, payload: res });
+  const { data } = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: data });
 }
