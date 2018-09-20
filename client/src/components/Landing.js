ce9070a4e0bc3800da83724a4e9302544c871220
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-20 01:02:49 -0500

    react-stripe-checkout

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>
+      Emaily!
+    </h1>
+    Collect feedback from your users
+  </div>
+);
+export default Landing;
