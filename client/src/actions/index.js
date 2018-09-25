d9b290902e1b87a6cfebfdf782ea66f8e1cf9c2d
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-24 20:13:07 -0500

    Reusing action types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,9 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
   const { data } = await axios.get('/api/current_user');
-
   dispatch({ type: FETCH_USER, payload: data });
+}
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  dispatch({ type: FETCH_USER, payload: res.data });
 }
