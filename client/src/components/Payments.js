f617de3eb6b0a04d8371bd7fdeea6d41a6c19f1d
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-24 20:16:16 -0500

    Positing the stripe tokens

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Payments extends Component {
   render() {
@@ -9,7 +11,7 @@ class Payments extends Component {
         description="$5 for 5 email credits"
         amount={5}
         currency="USD"
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add credits</button>
@@ -17,4 +19,4 @@ class Payments extends Component {
     );
   }
 }
-export default Payments;
+export default connect(null, actions)(Payments);
