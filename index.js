886f4593b2f4881119eb10ad63007d93dfedc3c1
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-25 14:15:03 -0500

    Creating a charge object

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,6 +1,13 @@
 const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
 module.exports = app => {
   app.post('/api/stripe', (req, res) => {
-
+    stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id
+    });
   });
 };
