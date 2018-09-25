97aa73ad490f57d09a6521f0ab00635ef2e601ee
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-25 14:43:26 -0500

    Requiring authentication

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,9 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must log in!' });
+    }
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
@@ -12,7 +15,7 @@ module.exports = app => {
     
     req.user.credits += 5;
     const user = await req.user.save();
-    
+
     res.send(user);
   });
 };
