554926ac9eefba0f8864f91d8336c37df84ae409
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-28 18:16:44 -0500

    Model deficiencies

--- models/Survey.js
+++ models/Survey.js
@@ -5,5 +5,7 @@ const surveySchema  = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String]
+  recipients: [String],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 }
 });
