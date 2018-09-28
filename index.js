68f571fec0088007cc851f0aae2be79411e94248
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-27 19:18:25 -0500

    added billing and client side app

--- index.js
+++ index.js
@@ -27,5 +27,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);// Is BETTER and great!
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res)  => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
