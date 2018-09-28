c5f974c0885b7110dde88fda13570b42544717de
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-28 18:35:28 -0500

    relationship fields

--- models/Survey.js
+++ models/Survey.js
@@ -9,6 +9,9 @@ const surveySchema  = new Schema({
   subject: String,
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateSent: Date,
+  lastResponded: Date
 });
 mongoose.model('surveys', surveySchema);
