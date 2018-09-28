7ef17803cb05e30fd22b396933423e679078fcd8
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-09-28 17:50:01 -0500

    Survey model

---
+++ models/Survey.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const surveySchema  = new Schema({
+  title: String,
+  body: String,
+  subject: String,
+  recipients: [String]
+});
