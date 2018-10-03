3623712d79d6af2be09d42723d388b34250e118d
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-03 16:33:12 -0500

    Creating surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,11 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {  
-
+    const { title, subject, body, recipients } = req.body;
   });
 };
