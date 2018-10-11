d5fdf53fcc42a079d6824dd0dfeeba4a7ca9bcfb
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-11 18:46:20 -0500

    Mailer constructor

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -16,5 +18,6 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+    const Mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -3,5 +3,14 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-  
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }  
 }
+
+module.exports = Mailer;
