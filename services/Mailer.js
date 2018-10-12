dcbe5a1fc503bd8b5bb4282f8f292cc26f919f2a
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-11 19:19:06 -0500

    Sending sendgrid emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,6 +6,8 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
+    this.sgApi = sendgrid(keys.sendGridKey);
+
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -13,6 +15,7 @@ class Mailer extends helper.Mail {
 
     this.addContent(this.body);
     this.addClickTracking();
+    this.addRecipients();
   }  
   formatAddresses(recipients) {
     return recipients.map(({ email }) => {
@@ -25,6 +28,23 @@ class Mailer extends helper.Mail {
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
+  }
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+    let response = this.sgApi.API(request);
+    return response;
   }
 }
 
