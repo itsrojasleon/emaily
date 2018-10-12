cf281f95ef8f6eebc1d8cd0882cba7407178510f
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-11 19:05:03 -0500

    boilerplaete for sending emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -10,7 +10,22 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
   }  
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
 }
 
 module.exports = Mailer;
