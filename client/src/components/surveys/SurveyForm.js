a0c358a2bf354829429b7b20b2837a73d77855dd
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-22 19:29:37 -0500

    redux from in practice

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
   render() {
     return (
-      <div>Survey Form</div>
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field
+            type="text"
+            name="surveyTitle"
+            component="input"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     );
   }
 }
