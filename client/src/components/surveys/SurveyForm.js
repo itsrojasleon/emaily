1e71a58d81176cb701a685be8f18b5886e3d2e55
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-23 18:14:11 -0500

    fields from config

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { reduxForm, Field } from 'redux-form';
 
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
+
 class SurveyForm extends Component {
-  renderFields = () => (
-    <div>
-      <Field 
-        label="Survey Title"
-        type="text" 
-        name="title" 
-        component={SurveyField} 
-      />
-      <Field 
-        label="Subject Line"
-        type="text" 
-        name="subject" 
-        component={SurveyField} 
-      />
-      <Field 
-        label="Email Body"
-        type="text" 
-        name="body" 
-        component={SurveyField} 
-      />
-      <Field 
-        label="Recipient List"
-        type="text" 
-        name="emails" 
-        component={SurveyField} 
-      />
-    </div>
-  );
+  renderFields = () => {
+    return _.map(FIELDS, ({ name, label }) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    });
+  }
   render() {
     return (
       <div>
