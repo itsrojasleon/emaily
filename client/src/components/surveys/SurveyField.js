27e49f43d16568cdf1fbef4d0979c80edb205a6a
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-23 17:56:16 -0500

    Wiring up Custom Fields

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,9 @@
+// SurveyField contains logic to render a single label and text input
+
+import React from 'react';
+
+export default ({ input }) => (
+  <div>
+    <input {...input} />
+  </div>
+);
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+import SurveyField from './SurveyField';
+
 class SurveyForm extends Component {
+  renderFields = () => (
+    <div>
+      <Field type="text" name="title" component={SurveyField} />
+    </div>
+  );
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field
-            type="text"
-            name="surveyTitle"
-            component="input"
-          />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
