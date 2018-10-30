337b06f15be173ce114a28983b5b0ea06162229e
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-30 17:26:13 -0600

    Redirect to /surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,8 +11,9 @@ export const handleToken = token => async dispatch => {
   dispatch({ type: FETCH_USER, payload: data });
 }
 
-export const submitSurvey = values => async dispatch => {
+export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post('/api/surveys', values);
-
+  
+  history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data })
 }
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { withRouter } from 'react-router-dom';
 import { FIELDS } from './formFields';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(FIELDS, ({ name, label }) => {
     return (
       <div key={name}>
@@ -27,7 +28,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
       >
         Back
       </button>
-      <button onClick={() => submitSurvey(formValues)} className="green darken-3 btn-flat right white-text">
+      <button onClick={() => submitSurvey(formValues, history)} className="green darken-3 btn-flat right white-text">
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -39,4 +40,4 @@ function mapStateToProps(state) {
     formValues: state.form.surveyForm.values,
   };
 }
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
