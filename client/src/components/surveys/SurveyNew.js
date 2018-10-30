47e4b1216b37b38346c11a454fd17947417527fa
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-30 15:03:28 -0600

    Dumping form values

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -20,4 +21,6 @@ class SurveyNew extends Component {
     );
   }
 }
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
