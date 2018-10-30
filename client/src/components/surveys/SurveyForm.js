76609f8e4f7395d5d2aa87e58cdde19785234525
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-10-30 15:07:17 -0600

    Fixing property names

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -34,7 +34,7 @@ class SurveyForm extends Component {
 const validate = (values) => {
   const errors = {};
 
-  errors.emails = validateEmails(values.emails || '');
+  errors.recipients = validateEmails(values.recipients || '');
   
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export const FIELDS = [
   { label: 'Survey Title', name: 'title' },
   { label: 'Subject Line', name: 'subject' },
   { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' }
+  { label: 'Recipient List', name: 'recipients' }
 ];
