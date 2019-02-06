9ada6bb2d581ba1d55ebbafe20d3eb4b625810e8
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-06 14:31:45 -0600

    whithout window = axios

--- client/src/index.js
+++ client/src/index.js
@@ -6,22 +6,18 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-
 import reducers from './reducers';
 
 import App from './components/App';
 
 import axios from 'axios';
-window.axios = axios;
+// window.axios = axios;
 
-const store = createStore(
-  reducers,
-  applyMiddleware(thunk)
-);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>
     <App />
-  </Provider>,    
+  </Provider>,
   document.getElementById('root')
 );
