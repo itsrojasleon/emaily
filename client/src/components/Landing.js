1d78c74bef0280453203db86410089256c6b7c65
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-08 20:17:19 -0600

    watch my surveys

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div style={{ textAlign: 'center' }}>
-    <h2 className="emaily">
-      Emaily!
-    </h2>
+    <h2 className="emaily">Emaily!</h2>
     <p className="collect">Collect feedback from your users</p>
+    <Link className="watch-my-surveys" to="/surveys">
+      Watch my Surveys
+    </Link>
   </div>
 );
 export default Landing;
