b60a885e4e8a500bf45e51b82c216014a254333b
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-02-09 14:43:49 -0600

    You need to login first

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import SurveyList from './surveys/SurveyList';
+import LoginFirst from './LoginFirst';
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div className="dashboard">
-        <div className="add-container">
-          <Link to="/surveys/new" className="add">
-            <i className="material-icons">add</i>
-          </Link>
-        </div>
-        <SurveyList />
+        {this.props.auth ? (
+          <React.Fragment>
+            <div className="add-container">
+              <Link to="/surveys/new" className="add">
+                <i className="material-icons">add</i>
+              </Link>
+            </div>
+            <SurveyList />
+          </React.Fragment>
+        ) : (
+          <LoginFirst />
+        )}
       </div>
     );
   }
 }
-
-export default Dashboard;
+const mapStateToProps = ({ auth }) => ({
+  auth
+});
+export default connect(mapStateToProps)(Dashboard);
---
+++ client/src/components/LoginFirst.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginFirst() {
+  return (
+    <div className="login-first">
+      <p className="how-is-it-going">How's it going!</p>
+      <p className="you-need">You need to login first</p>
+    </div>
+  );
+}
+export default LoginFirst;
